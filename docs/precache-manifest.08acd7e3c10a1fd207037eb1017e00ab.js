f1bc871534b3bf040e27c892fc4b7060241b78ea
Martin Rivière <martin.riviere@gmail.com>
2019-11-22 00:46:43 +0100

    deploy app

---
+++ docs/precache-manifest.08acd7e3c10a1fd207037eb1017e00ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10610f57218430837e68a4f237c657bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927f10ec5278201c1393",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b37f299f94b9803cd675",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "927f10ec5278201c1393",
+    "url": "/static/js/2.7c2edde8.chunk.js"
+  },
+  {
+    "revision": "b37f299f94b9803cd675",
+    "url": "/static/js/main.e55ea563.chunk.js"
+  },
+  {
+    "revision": "2d9a60d7c65c67fd25bb",
+    "url": "/static/js/runtime-main.24431120.js"
+  }
+]);
